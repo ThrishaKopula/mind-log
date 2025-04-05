b1bed03515f528cf0e266de534a507140227136e
Thrisha Kopula <thrishak1@gmail.com>
2025-04-05 11:39:32 -0700

    addUserForm

--- client/src/getUser/User.jsx
+++ client/src/getUser/User.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./user.css"
+import axios from "axios";
+import { Link } from 'react-router-dom';
 
 const User = () => {
+    const [users, setUsers] = useState([])
+    useEffect(()=> {
+        const fetchData = async()=>{
+            try {
+                const response = await axios.get("http://localhost:8000/api/users");
+                setUsers(response.data);
+            } catch (error) {
+                console.log("Error while fetching data", error);
+            }
+        };
+        fetchData();
+    }, []);
+
   return (
     <div className='userTable'>
-        <button type="button" class="btn btn-primary">
+        <Link to="/add" type="button" class="btn btn-primary">
             Add User 
             <i class="fa-solid fa-user-plus"></i>
-        </button>
+        </Link>
         <table className='table table-bordered'>
             <thead>
                 <tr>
@@ -19,21 +34,26 @@ const User = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>John</td>
-                    <td>john@gmail.com</td>
-                    <td>US</td>
-                    <td className="actionButtons">
-                        <button type="button" class="btn btn-info">
-                            <i class="fa-solid fa-pen-to-square"></i>
-                        </button>
-                        <button type="button" class="btn btn-danger">
-                            <i class="fa-solid fa-trash"></i>
-                        </button>
-                        
-                    </td>
-                </tr>
+                {users.map((user, index)=>{
+                   return (
+                    <tr>
+                        <td>{index+1}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.address}</td>
+                        <td className="actionButtons">
+                            <button type="button" class="btn btn-info">
+                                <i class="fa-solid fa-pen-to-square"></i>
+                            </button>
+                            <button type="button" class="btn btn-danger">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                            
+                        </td>
+                    </tr>
+                   );  
+                })}
+                
             </tbody>
         </table>
     </div>
